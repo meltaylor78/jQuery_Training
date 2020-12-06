289867ca2a50d2e927091ac37547271ef34bc9ad
meltaylor78 <meltaylor78@gmail.com>
2020-12-06 20:21:22 +0000

    added colours to rows using odd/even selectors in jQuery. Fixed issue with linking of script file to html

--- js/my_script.js
+++ js/my_script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
     $("h2").addClass("underline"); // underline all h2 elements
     $("ul").addClass("border"); // add a border to the all lists.
+    $("tr:odd").addClass("odd");
+    $("tr:even").addClass("even");
 });
